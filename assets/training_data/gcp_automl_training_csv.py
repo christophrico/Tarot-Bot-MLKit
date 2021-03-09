1f1ae840e77ebac86d7d16f905e9b9a61893bfee
Christophrico <christoph.rico@gmail.com>
2021-03-09 14:56:39 -0800

    Updated format to Black.

--- assets/training_data/gcp_automl_training_csv.py
+++ assets/training_data/gcp_automl_training_csv.py
@@ -8,10 +8,10 @@ Images for each class should be sorted into a folder named for each class,
 Run this script from the same directory as the folders.
 """
 
-#change this to the base path of the GCP bucket where your images are
+# change this to the base path of the GCP bucket where your images are
 base_gcp_path = "gs://christophrico-462-classification-bucket"
 
-#get all the names of the class folders
+# get all the names of the class folders
 class_folders = []
 path = "./"
 p = os.listdir(path)
@@ -19,29 +19,26 @@ for i in p:
     if os.path.isdir(i):
         class_folders.append(i)
 
-#now go through one by one and generate csv of path and class
-#csv format:
+# now go through one by one and generate csv of path and class
+# csv format:
 # set, image_path, class_label
-df=pd.DataFrame(columns=['set', 'path', 'label'])
+df = pd.DataFrame(columns=["set", "path", "label"])
 
-#within each class folder
+# within each class folder
 for img_class in class_folders:
     folder = "{}/{}".format(path, img_class)
-    #get the names of the pics
+    # get the names of the pics
     image_names = os.listdir(folder)
-    #for each pic
+    # for each pic
     for image in image_names:
-        #generate the gcp path
+        # generate the gcp path
         gcp_image_path = "{}/{}/{}".format(base_gcp_path, img_class, image)
-        #append it to the df
-        df = df.append({
-                        "set" : "UNASSIGNED",
-                        "path" : gcp_image_path,
-                        "label" : img_class
-                      },
-                      ignore_index = True
-                    )
+        # append it to the df
+        df = df.append(
+            {"set": "UNASSIGNED", "path": gcp_image_path, "label": img_class},
+            ignore_index=True,
+        )
 
-#now save the csv
+# now save the csv
 filename = "classifier_training_data.csv"
 df.to_csv(filename)
